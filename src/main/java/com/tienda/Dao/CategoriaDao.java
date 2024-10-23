7d6807055ccfb3ab0e4ec30cfb685c34f93adda8
Alecast1227 <castilloarroyoalejandro@gmail.com>
2024-10-22 21:16:26 -0600

    Sem 6

--- src/main/java/com/tienda/Dao/CategoriaDao.java
+++ src/main/java/com/tienda/Dao/CategoriaDao.java
@@ -1,13 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package com.tienda.Dao;
+
+package com.tienda.dao;
 
 import com.tienda.domain.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-public interface CategoriaDao extends JpaRepository<Categoria , long>{
+public interface CategoriaDao extends JpaRepository<Categoria, Long>{
     
 }
