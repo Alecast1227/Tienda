b5a416c0448548bd08dc0a0946679b45399fa633
Alecasttt <alejandrocastilloarroyo1227@gmail.com>
2024-12-03 20:49:52 -0600

    semana 12

--- src/main/java/com/tienda/ProjectConfig.java
+++ src/main/java/com/tienda/ProjectConfig.java
@@ -4,10 +4,46 @@
  */
 package com.tienda;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
 /**
  *
  * @author casti
  */
 public class ProjectConfig {
     
+
+@Bean
+public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+	http
+			.authorizeHttpRequests((request) -> request
+			.requestMatchers("/", "/index", "/errores/**",
+					"/carrito/**", "/reportes/**",
+					"/registro/**", "/js/**", "/webjars/**", "/error", "/refrescarBoton")
+			.permitAll()
+			.requestMatchers(
+					"/producto/nuevo", "/producto/guardar",
+					"/producto/modificar/**", "/producto/eliminar/**",
+					"/categoria/nuevo", "/categoria/guardar",
+					"/categoria/modificar/**", "/categoria/eliminar/**",
+					"/usuario/nuevo", "/usuario/guardar",
+					"/usuario/modificar/**", "/usuario/eliminar/**",
+					"/reportes/**", "/pruebas/**"
+			).hasRole("ADMIN")
+			.requestMatchers(
+					"/producto/listado",
+					"/categoria/listado",
+					"/usuario/listado"
+			).hasAnyRole("ADMIN", "VENDEDOR")
+			.requestMatchers("/facturar/carrito")
+			.hasRole("USER")
+			)
+			.formLogin((form) -> form
+			.loginPage("/login").permitAll())
+			.logout((logout) -> logout.permitAll());
+	return http.build();
+}
+
 }
